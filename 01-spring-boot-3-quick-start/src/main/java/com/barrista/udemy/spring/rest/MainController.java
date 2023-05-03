e9edf24ed21e17f5f233cb18e26c7e8f27310456
MaxBarrista <79321066+MaxBarrista@users.noreply.github.com>
2023-05-03 18:05:02 +0300

    feat: tested autowiring through setter

--- 01-spring-boot-3-quick-start/src/main/java/com/barrista/udemy/spring/rest/MainController.java
+++ 01-spring-boot-3-quick-start/src/main/java/com/barrista/udemy/spring/rest/MainController.java
@@ -1,5 +1,7 @@
 package com.barrista.udemy.spring.rest;
 
+import com.barrista.udemy.spring.service.DummyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,20 @@ public class MainController {
   @Value("${custom.value1}")
   private String customValue1;
 
-  @GetMapping()
+  private DummyService dummyService;
+
+  @Autowired
+  public void setDummyService(DummyService dummyService) {
+    this.dummyService = dummyService;
+  }
+
+  @GetMapping
   public String greeting() {
-    return "Hello world! Custom value: " + customValue1;
+    return "Hello world! " + customValue1;
+  }
+
+  @GetMapping("foo")
+  public String foo() {
+    return dummyService.foo();
   }
 }
---
+++ 01-spring-boot-3-quick-start/src/main/java/com/barrista/udemy/spring/service/DummyService.java
@@ -0,0 +1,10 @@
+package com.barrista.udemy.spring.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DummyService {
+  public String foo() {
+    return "foo";
+  }
+}
