fa51dec4cbd5bc6eaa506c7f9a2b5921f08b48fd
MaxBarrista <79321066+MaxBarrista@users.noreply.github.com>
2023-05-03 17:56:18 +0300

    feat: add explicit package scan definition

--- 01-spring-boot-3-quick-start/src/main/java/com/barrista/udemy/spring/Application.java
+++ 01-spring-boot-3-quick-start/src/main/java/com/barrista/udemy/spring/Application.java
@@ -3,7 +3,7 @@ package com.barrista.udemy.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.barrista.udemy.spring"})
 public class Application {
 
 	public static void main(String[] args) {
